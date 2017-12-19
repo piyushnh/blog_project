0390c08cee5d7cbf52054cc59cddadbb15ed7b16
piyushnh <piyush.n.h@gmail.com>
2017-12-19 11:03:53 +0530

    80% done. DEleting and drafts list not working

--- medium/blog/models.py
+++ medium/blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete = models.CASCADE)
     title = models.CharField(max_length = 200)
     text = models.TextField()
-    created_date = models.DateTimeField(default = timezone.now())
+    created_date = models.DateTimeField(default = timezone.now)
     published_date = models.DateTimeField(blank = True, null = True)
 
     def publish(self):
@@ -18,7 +18,7 @@ class Post(models.Model):
         return self.comments.filter(approved_comment = True)
 
     def get_absolute_url(self):
-        return reverse("post_detail", kwargs = {'pk':self.pk})
+        return reverse("blog:post_detail", kwargs = {'pk':self.pk})
 
     def __self__(self):
         return self.title
@@ -27,7 +27,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post,related_name = 'comments', on_delete = models.CASCADE )
     author = models.CharField(max_length = 100)
     text = models.TextField()
-    created_date = models.DateTimeField(default = timezone.now())
+    created_date = models.DateTimeField(default = timezone.now)
     approved_comment = models.BooleanField(default = False)
 
     def approve(self):
@@ -35,7 +35,7 @@ class Comment(models.Model):
         self.save()
 
     def get_absolute_url(self):
-        return reverse("post_list")
+        return reverse("blog:post_list")
 
     def __str__(self):
         return self.text
--- medium/blog/views.py
+++ medium/blog/views.py
@@ -19,9 +19,10 @@ class PostListView(ListView):
     template_name = 'blog/post_list.html'
 
     def get_queryset(self):
-        return Post.objects,filter(published_date__lte = timezone.now()).order_by('-published_date')
+        return Post.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')
 
 class PostDetailView(DetailView):
+    context_object_name = 'my_post'
     model = Post
     template_name = 'blog/post_detail.html'
 
@@ -29,6 +30,7 @@ class PostDetailView(DetailView):
 class CreatePostView(LoginRequiredMixin, CreateView):
     login_url = '/login/'
     redirect_field_name = 'blog/post_detail.html'
+    template_name = 'blog/post_form.html'
 
     model = Post
     form_class = PostForm
@@ -41,9 +43,6 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
     form_class = PostForm
 
 class PostDeleteView(LoginRequiredMixin, DeleteView):
-    login_url = '/login/'
-    #NOTE not sure of adding this field.Experimentation
-    redirect_field_name = 'blog/post_list.html'
 
     model = Post
     success_url = reverse_lazy('post_list')
@@ -56,14 +55,14 @@ class DraftListView(LoginRequiredMixin,ListView):
     model = Post
     template_name = 'blog/post_draft_list.html'
     def get_queryset(self):
-        return Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
 
 @login_required
 def post_publish(request,pk ):
     post = get_object_or_404(Post, pk = pk)
     post.publish()
     # NOTE THAT I HAVE USED POST.PK HERE
-    return redirect('post_detail', pk = post.pk)
+    return redirect('blog:post_detail', pk = post.pk)
 
 ###########################################################################
 # COMMENT MECHANISM STARTS HERE
@@ -72,13 +71,13 @@ def post_publish(request,pk ):
 @login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk = pk)
-    if request.method == POST:
+    if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit = False)
             comment.post = post
             comment.save()
-            return redirect('post_detail', pk = post.pk)
+            return redirect('blog:post_detail', pk = post.pk)
     else:
         form = CommentForm()
     return render(request, 'blog/comment_form.html', {'form':form})
@@ -88,11 +87,11 @@ def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     comment.save()
-    return redirect('post_detail', pk = comment.post.pk)
+    return redirect('blog:post_detail', pk = comment.post.pk)
 
 @login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     post_pk = comment.post.pk
     comment.delete()
-    return redirect('post_detail', pk = post_pk)
+    return redirect('blog:post_detail', pk = post_pk)
